a692111d3b6800e8ef59f0e859683456c98880c6
Minipeps <mpcaudan@gmail.com>
2024-11-30 11:47:44 +0100

    player: update control physics

--- 2D/Player/player2D.gd
+++ 2D/Player/player2D.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-const SPEED = 1000.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 200.0
+const JUMP_VELOCITY = 200
+const FRICTION = 40
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,22 +15,36 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func _physics_process(delta):
+	self._handleMovement(delta)
+
 func _handleMovement(delta):
+	# Add gravity
+	self.velocity.y += gravity * delta
+	
 	# Handle jump.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		self.velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("player_move_up") and is_on_floor():
+		self.velocity.y = -JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	var horizontal_input = Input.get_action_strength("player_move_right") - Input.get_action_strength("player_move_left")
-	var moveVector = Vector2(horizontal_input, 0).normalized()
-	var direction = moveVector.normalized()
-	if direction:
-		self.velocity.x = direction.x * SPEED * delta
-	else:
-		self.velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-	print(velocity)
+	var direction = Vector2(horizontal_input, 0).normalized()
+	self.velocity.x += direction.x * SPEED * delta
+	
+	# Apply user input and update floor_normal
 	var collider = self.move_and_slide()
-
-func _physics_process(delta):
-	self._handleMovement(delta)
+	
+	# Accelerate/deccelerate on slopes
+	var floor_normal = get_floor_normal()
+	if abs(floor_normal.x) > 1e-3:
+		print(floor_normal)
+		self.velocity.x += floor_normal.x * SPEED * delta
+	# On flat terrain, apply friction
+	else:
+		apply_friction(delta)
+	
+func apply_friction(delta):
+	if (velocity.x > 0):
+		self.velocity.x -= FRICTION * delta
+	elif (velocity.x < 0):
+		self.velocity.x += FRICTION * delta
