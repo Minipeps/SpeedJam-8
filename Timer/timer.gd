63c15b49f8f3521d804b2d2085bbae7c2b42e401
Vynrae06 <ghaith.elghaddaf@gmail.com>
2024-11-30 12:05:55 +0100

    Basic Timer

---
+++ Timer/timer.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var TIME_ELAPSED : float = 0.0
+var UPDATE_TIMER : bool
+
+func _ready():
+	start_timer()
+
+func _process(delta):
+	update_timer(delta)
+	update_timer_label(TIME_ELAPSED)
+
+func start_timer():
+	UPDATE_TIMER = true
+	
+func update_timer(delta):
+	if UPDATE_TIMER:
+		TIME_ELAPSED += delta
+	
+func stop_and_reset_timer():
+	TIME_ELAPSED = 0.0
+	UPDATE_TIMER = false
+
+func update_timer_label(time_elapsed):
+	var seconds : int = fmod(time_elapsed,60.0)
+	var minutes : int = time_elapsed / 60.0
+	$Label.text = "%02d:%02d" % [minutes, seconds]
