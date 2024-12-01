d6e3b53e1fb9fd2a2b85098ef5a218e1d363a9d7
LucasDubouchet <59601961+LucasDubouchet@users.noreply.github.com>
2024-12-01 18:36:15 +0100

    checkpoint changes color

--- Checkpoint/checkpoint.gd
+++ Checkpoint/checkpoint.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	$AnimatedSprite2D.play()
+	$AnimatedSprite2D.play("redflag")
 
 func _on_area_2d_body_entered(body):
+	$AnimatedSprite2D.play("greenflag")
 	get_owner().register_checkpoint(self)
